58769d765637baf354f21f71fe33cf466dcb087b
raphael.nogueira@5by5.com.br <raphael.nogueira@5by5.com.br>
2020-03-04 15:12:48 -0300

    Adjust login authentication and implements parcial cache

--- MyAppCQRS.Domain.Core/Responses/Response.cs
+++ MyAppCQRS.Domain.Core/Responses/Response.cs
@@ -13,5 +13,11 @@ namespace MyAppCQRS.Domain.Core.Responses
 
         public object Data { get; private set; }
         public bool IsValid { get; private set; }
+
+        public T Convert<T>()
+        {
+            var jsonString = JsonConvert.SerializeObject(Data, Formatting.Indented);
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
     }
 }
