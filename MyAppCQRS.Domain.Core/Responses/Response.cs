28995bd34fe3a77d3ab522d6155fbd479cb8c093
raphael.nogueira@5by5.com.br <raphael.nogueira@5by5.com.br>
2020-03-18 11:00:21 -0300

    Adjust and implements logout

--- MyAppCQRS.Domain.Core/Responses/Response.cs
+++ MyAppCQRS.Domain.Core/Responses/Response.cs
@@ -3,9 +3,9 @@ using Newtonsoft.Json;
 
 namespace MyAppCQRS.Domain.Core.Responses
 {
-    public class Response : JsonParse
+    public class Response
     {
-        public Response(object response, bool isValid)
+        public Response(object response, bool isValid = true)
         {
             Data = response;
             IsValid = isValid;
@@ -13,11 +13,5 @@ namespace MyAppCQRS.Domain.Core.Responses
 
         public object Data { get; private set; }
         public bool IsValid { get; private set; }
-
-        public T Convert<T>()
-        {
-            var jsonString = JsonConvert.SerializeObject(Data, Formatting.Indented);
-            return JsonConvert.DeserializeObject<T>(jsonString);
-        }
     }
 }
