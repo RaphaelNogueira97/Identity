f336ae01b62567bbb722fa7c7d6960648552f918
raphael.nogueira@5by5.com.br <raphael.nogueira@5by5.com.br>
2020-03-18 11:21:49 -0300

    Adjust responses, login and base classes

--- MyAppCQRS.Domain.Core/Entities/BaseClass.cs
+++ MyAppCQRS.Domain.Core/Entities/BaseClass.cs
@@ -9,20 +9,11 @@ namespace MyAppCQRS.Domain.Core.Entities
         {
             return JsonSerializer.Serialize("");
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> adjust and implementation generic classes, but is broken application
 
         public virtual T ParseJson()
         {
             var jsonString = JsonSerializer.Serialize(this);
             return JsonSerializer.Deserialize<T>(jsonString);
         }
-<<<<<<< HEAD
-=======
->>>>>>> Adjust login authentication and implements parcial cache
-=======
->>>>>>> adjust and implementation generic classes, but is broken application
     }
 }
--- MyAppCQRS.Domain.Core/Responses/ResponseRepository.cs
+++ MyAppCQRS.Domain.Core/Responses/ResponseRepository.cs
@@ -1,24 +1,16 @@
 ﻿using MyAppCQRS.Domain.Core.Interfaces;
 using MyAppCQRS.Domain.Core.Responses;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace MyAppCQRS.Infra.Responses
 {
     public class ResponseRepository : IResponseService
     {
-<<<<<<< HEAD
         public Response CreateFailResponse()
         {
             return new Response(null, false);
         }
-
         public Response CreateResponse(object response, bool isValid)
         {
-=======
-        public Response CreateResponse(object response, bool isValid)
-        {
->>>>>>> Adjust login authentication and implements parcial cache
             return new Response(response, isValid);
         }
     }
